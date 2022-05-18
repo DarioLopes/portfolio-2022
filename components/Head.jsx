fe3775a3a1b16222262916a86a451884b5798e60
Dario Lopes <dariolopes@MBP-de-Dario.fritz.box>
2022-05-18 04:50:45 +0200

    Head + meta update

--- components/Head.jsx
+++ components/Head.jsx
@@ -5,6 +5,9 @@ function globalHead(props) {
     <Head>
       <title>Dario Lopes{props.title ? ` | ${props.title} | ` : ` | `}Front End Developer | UI/UX Designer</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content="Portfolio of Dario Lopes a Front-end developer-UI/UX Designer based in Belgium." />
+      <meta name="theme-color" content="#161032" media="(prefers-color-scheme: light)" />
+      <meta name="theme-color" content="#161032" media="(prefers-color-scheme: dark)" />
       <link rel="shortcut icon" href={'/img/favicon.png'} type="image/x-icon" />
       {props.children}
     </Head>
