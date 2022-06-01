68ec65df66738dc8966f082b5858ca087e81b5fe
Dario Lopes <dariolopes@MacBook-Pro-de-Dario.local>
2022-06-01 02:29:14 +0200

    bg opacity fix on load

--- components/Background.jsx
+++ components/Background.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Background = (props) => {
   return (
     <div className="bg-container-main">
-      <span className="bg">
+      <span className="bg" style={{ opacity: 0.2 }}>
         <Image layout="fill" src={props.src ? props.src : '/img/bg-skill.jpg'} alt={props.alt ? props.alt : 'Background image'} priority="true" />
       </span>
     </div>
